3b7df3a6c20ac0453f39bde69573623c331ad681
Anshul Singh <anshulspidy@gmail.com>
2022-03-08 06:40:17 +0530

    xor1 working

--- xor1.asm
+++ xor1.asm
@@ -1,13 +1,64 @@
-section     .text
-global      _start 
+section .data
+    getplaintext db "Enter the text to be encrypted"  ; max input length: 100
+    len1 equ $ - getplaintext
+    getkey db "Enter the xor key"                     ; single char only
+    len2 equ $ - getkey
+
+section .bss
+    plaintext resb 100 ; reserve 100 bytes for plaintext to be encrypted
+    key resb 1         ; reserve 1 byte for xor key
+
+section .text
+    global _start
+
 _start: 
-    mov     edx,len   
-    mov     ecx,msg   
-    mov     ebx,1   
-    mov     eax,4   
-    int     0x80   
-    mov     eax,1  
-    int     0x80   
-section     .data
-msg     db  'Hello world',0xa  
-len     equ $ - msg   
+    call _printText1
+    call _input1
+    call _printText2
+    call _input2
+
+    mov esi, key
+    mov edi, plaintext
+    mov ecx, len1
+
+    xorloop:
+        lodsb
+        xor al, [key]
+        stosb
+        loop xorloop
+        
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+_printText1:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, getplaintext
+    mov rdx, len1
+    syscall
+    ret
+
+_input1:
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, plaintext
+    mov rdx, 100
+    syscall
+    ret
+
+_printText2:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, getkey
+    mov rdx, len2
+    syscall
+    ret
+
+_input2:
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, key
+    mov rdx, 1
+    syscall
+    ret
