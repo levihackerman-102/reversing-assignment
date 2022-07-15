074e805260393854457f4aef8f50294546c9f602
Anshul Singh <anshulspidy@gmail.com>
2022-07-15 10:54:16 +0530

    Rename files

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nasm -f elf64 xor1.asm # assemble the program  
-ld -s -o xor1 xor1.o # link the object file nasm produced into an executable file  
-./xor1 # xor1 is an executable file
+nasm -f elf64 xor-cipher.asm # assemble the program  
+ld -s -o xor-cipher xor-cipher.o # link the object file nasm produced into an executable file  
+./xor-cipher # xor-cipher is an executable file
